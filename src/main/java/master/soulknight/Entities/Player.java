373158e2870e7a3d8b73aacc7c68368131571176
Nam <namnguyenly2k1@gmail.com>
2021-11-10 22:03:34 +0700

    Moving player
    player can now move

--- src/main/java/master/soulknight/Entities/Player.java
+++ src/main/java/master/soulknight/Entities/Player.java
@@ -55,11 +55,15 @@ public class Player extends Entity {
             placeBoom = false;
         }
     }
-
     public void update() {
         super.update();
         if (!fallen) {
             move();
+            if (left || right) {
+                pos.x += dx;
+            } else if (up || down) {
+                pos.y += dy;
+            }
         }
     }
 
