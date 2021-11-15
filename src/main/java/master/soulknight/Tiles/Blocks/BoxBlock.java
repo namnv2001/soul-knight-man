7b2e5a2ebaa5e36a901dd1aeb649bfec210967ae
Haru <hninh1710@gmail.com>
2021-11-15 15:54:17 +0700

    update

---
+++ src/main/java/master/soulknight/Tiles/Blocks/BoxBlock.java
@@ -0,0 +1,28 @@
+package master.soulknight.Tiles.Blocks;
+
+import javafx.scene.image.Image;
+import master.soulknight.Graphics.Sprite;
+import master.soulknight.Util.AABB;
+import master.soulknight.Util.Vector2f;
+
+public class BoxBlock extends Block{
+
+    public BoxBlock(int width, int height, Image img, Vector2f pos) {
+        super(width, height, img, pos);
+    }
+
+    @Override
+    public boolean update(AABB p) {
+        return false;
+    }
+
+    @Override
+    public boolean isInside(AABB p) {
+        return false;
+    }
+
+    @Override
+    public Sprite getImage() {
+        return null;
+    }
+}
---
+++ src/main/java/master/soulknight/Tiles/Blocks/FloorBlock.java
@@ -0,0 +1,29 @@
+package master.soulknight.Tiles.Blocks;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import master.soulknight.Graphics.Sprite;
+import master.soulknight.Util.AABB;
+import master.soulknight.Util.Vector2f;
+
+public class FloorBlock extends Block{
+    public FloorBlock(int width, int height, Image img, Vector2f pos) {
+        super(width, height, img, pos);
+    }
+
+    @Override
+    public boolean update(AABB p) {
+        return false;
+    }
+
+    @Override
+    public boolean isInside(AABB p) {
+        return false;
+    }
+
+    @Override
+    public Sprite getImage() {
+        return null;
+    }
+
+}
