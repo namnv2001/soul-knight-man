9f3dfb258d6f3b2b90a0940d6bee24fdc96d4f04
Haru <hninh1710@gmail.com>
2021-12-09 15:19:24 +0700

    commit

--- src/main/java/master/soulknight/States/GameOverState.java
+++ src/main/java/master/soulknight/States/GameOverState.java
@@ -37,7 +37,6 @@ public class GameOverState extends GameState {
         gc.drawImage(image, 0, 0);
         // x,y pos in new Vector2f(x,y)
         SpriteSheet.drawArray(gc, font, "Player (1): " + TileManager.score, new Vector2f(302,70),40,55);
-
     }
 
     @Override
